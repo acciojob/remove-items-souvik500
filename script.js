14db3dedd2006e04c4a8df54770962ec205fae14
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-17 13:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function removeSelectedOption() {
+  // Get the dropdown list element
+  const colorSelect = document.getElementById('colorSelect');
+
+  // Remove the selected option from the dropdown list
+  colorSelect.remove(colorSelect.selectedIndex);
+}
